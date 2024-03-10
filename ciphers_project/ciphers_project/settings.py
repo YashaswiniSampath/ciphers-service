c162341f99009e8d9c0bc1eaff99b8520601cb12
Yashaswini Sampath Kumar <44898518+YashaswiniSampath@users.noreply.github.com>
2024-03-09 21:22:07 -0500

    Update settings.py

--- ciphers_project/ciphers_project/settings.py
+++ ciphers_project/ciphers_project/settings.py
@@ -133,7 +133,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    "https://yashaswinisampath.github.io/numbers_app/",
     "http://localhost:3000",
     "https://yashaswinisampath.github.io",
 ]
